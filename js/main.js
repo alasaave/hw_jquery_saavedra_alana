30a609010a26b3a2d33c81b55122b34fd0fbc81a
Alana Saavedra <alasaave@gmail.com>
2018-04-04 22:07:09 -0500

    alter p tags using .css jQuery & object

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 //Jquery
 $(document).ready(function () {
-	//get all p elements and assign to var "paragraphs"
-	var paragraphs = $('p');
+    //get all p elements and assign to var "paragraphs"
+    var paragraphs = $('p');
+    //pass object (containing 3 name/value pairs) to css jQuery altering paragraph stylesheet
+    $(paragraphs).css({
+        'color': 'blue',
+        'font-style': 'italic',
+        'background-color': 'whitesmoke'
+    });
 });
